de8acf85e46298cfbf5d64a1825954b652533b48
Andrzej Rybczak <electricityispower@gmail.com>
2015-05-06 17:10:29 +0200

    initial commit

---
+++ src/Control/Monad/Time.hs
@@ -0,0 +1,11 @@
+module Control.Monad.Time (MonadTime(..)) where
+
+import Control.Monad.Trans
+import Data.Time
+
+-- | Class of monads which carry the notion of the current time.
+class Monad m => MonadTime m where
+  currentTime :: m UTCTime
+
+instance MonadTime IO where
+  currentTime = getCurrentTime
---
+++ src/Control/Monad/Time/Instances.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, OverlappingInstances
+  , UndecidableInstances #-}
+module Control.Monad.Time.Instances () where
+
+import Control.Monad.Trans
+
+import Control.Monad.Time
+
+-- | Generic, overlapping instance.
+instance (
+    MonadTime m
+  , MonadTrans t
+  , Monad (t m)
+  ) => MonadTime (t m) where
+    currentTime = lift currentTime
